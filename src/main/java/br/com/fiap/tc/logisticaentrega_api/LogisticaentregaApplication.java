4c831f349a13f4262f351ad1ba5dd06e271e0207
MarcosHRFerreira <marcoshrferreira@gmail.com>
2024-11-01 09:15:34 -0300

    carga inicial

---
+++ src/main/java/br/com/fiap/tc/logisticaentrega_api/LogisticaentregaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tc.logisticaentrega_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaentregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaentregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/tc/logisticaentrega_api/LogisticaentregaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tc.logisticaentrega_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaentregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
